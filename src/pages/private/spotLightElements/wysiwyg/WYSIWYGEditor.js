d151fdf5f5671a97c53f36d2f7fae7e7fa8e2f7c
Brad Brous <brbr51@tampabay.rr.com>
2020-11-17 02:19:23 -0500

    wysiwg styles and options enabled

--- src/pages/private/spotLightElements/wysiwyg/WYSIWYGEditor.js
+++ src/pages/private/spotLightElements/wysiwyg/WYSIWYGEditor.js
@@ -45,6 +45,32 @@ const WYSIWYGEditor = props => {
           wrapperClassName="wrapper-class"
           editorClassName="editor-class"
           onEditorStateChange={onEditorStateChange}
+          
+          // toolbar={{
+          //   inline: { inDropdown: true },
+          //   list: { inDropdown: true },
+          //   textAlign: { inDropdown: true },
+          //   link: { inDropdown: true },
+          //   history: { inDropdown: true },
+          // }}
+
+          toolbar ={
+            {
+            options:['inline','colorPicker', 'link', 'list',  ], // This is where you can specify what options you need in
+            //the toolbar and appears in the same order as specified
+            inline:
+            {
+            options:['bold', 'italic', 'underline'] // this can be specified as well, toolbar wont have
+            //strikethrough, 'monospace', 'superscript', 'subscript'
+            },
+   
+            }
+            }
+
+
+
+
+
         />
       </div>
     </React.Fragment>
