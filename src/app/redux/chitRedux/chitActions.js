136d0b3cc32378137316072220004b84f94afcbf
Brad Brous <brbr51@tampabay.rr.com>
2021-06-21 11:35:15 -0400

    reselect try store setup

---
+++ src/app/redux/chitRedux/chitActions.js
@@ -0,0 +1,42 @@
+// chit Actions
+
+
+import {
+  
+  DUMMY_CONSTANT, 
+
+  
+} from '.chitConstants';
+
+
+
+// export const  openCloseSidePanel = (sidePanelState) => {
+//   console.log("[ACTION] - clicked : state is: ", sidePanelState)
+//   return {
+//     type: OPEN_CLOSE_SIDE_PANEL,
+//     payload: {
+//       displaySidePanel: sidePanelState
+      
+//     }
+//   }
+// }
+
+// export const  updateSpotLightView = () => {
+  
+//   return {
+//     type: SHOW_SPOTLIGHT,
+//     payload: {
+       
+//       spotLightViews: {
+
+
+       
+//       defaultSpotLight: 'spot1',
+//       displayedSpotLightId: 'all',
+//       spotLightsArray: ['spot1','spot2','spot3','spot4','spot5'],  
+//       }
+      
+//     }
+//   }
+}
+
